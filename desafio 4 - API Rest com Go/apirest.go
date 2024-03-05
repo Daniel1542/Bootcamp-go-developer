d09a6b6653f539f87db5c69f330e08c4eafacf3e
Daniel Henrique <queirozdaniel1999@gmail.com>
2024-03-05 00:15:50 -0300

    update

--- desafio 4 - API Rest com Go/apirest.go
+++ desafio 4 - API Rest com Go/apirest.go
@@ -127,7 +127,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/contato", GetPeople).Methods("GET")
 	router.HandleFunc("/contato/{id}", GetPerson).Methods("GET")
-	router.HandleFunc("/contato/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/contato", CreatePerson).Methods("POST")
 	router.HandleFunc("/contato/{id}", UpdatePerson).Methods("PUT")
 	router.HandleFunc("/contato/{id}", DeletePerson).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", router))
